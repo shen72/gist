b86db2a3e845518b982ac5b0323fcdd830862b4f
shen72 <shen72@gmail.com>
2012-11-25 20:09:23 +0800

    fix a command line bug

--- extColorGist.cc
+++ extColorGist.cc
@@ -1,24 +1,29 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
+#include <fstream>
 
 #include "colorgist.h"
 #include "macros.h"
 
 DEFINE_string(in, "", "input image");
-DEFINE_string(out, "", "output image");
-
+DEFINE_string(out, "/dev/stdout", "output image");
 
 using namespace std;
 
 int main(int argc, char* argv[]) {
   google::ParseCommandLineFlags(&argc, &argv, true);
   
-  cv::Mat img(cv::imread(argv[1]));
+  cv::Mat img(cv::imread(FLAGS_in));
   
   ColorGistFeature feature = ColorGistFeature::extract(img); 
 
-  cout << ".";
+  ofstream fout(FLAGS_out);
+
+  CHECK(fout) << "Unable to open " << FLAGS_out << endl;
+
+  fout << ".";
   for (size_t i = 0; i < feature.dim(); i++)
-    cout << "\t" << feature[i];
-  cout << std::endl;
+    fout << "\t" << feature[i];
+  fout << endl;
+  fout.close();
 }
